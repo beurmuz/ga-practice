5b10b5769b09c193e29dd430f1f7730dffd5b3c9
beurmuz <63042544+beurmuz@users.noreply.github.com>
2023-06-20 15:34:59 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,14 +1,13 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = 'https://ridibooks.com/category/new-releases/2200'
+url = 'https://vibe.naver.com/artist/331653/tracks?artistTrackType=RELEASE'
 response = requests.get(url)
 response.encoding = 'utf-8'
 html = response.text
 
 soup = BeautifulSoup(html, 'html.parser')
 
-#__next > main > div > section > ul.fig-1rl9mz1 > li:nth-child(1) > div > div.fig-smbj6f > div > div:nth-child(1) > a
-bookservices = soup.select('.fig-rs5q24')
-for no, book in enumerate(bookservices, 1):
-    print(no, book.text.strip())
+songs = soup.select('.song')
+for no, title in enumerate(songs, 1):
+    print(no, title.text.strip())
