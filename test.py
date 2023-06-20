9aabb0312213f7ffd680f01849e8f50494414a28
beurmuz <63042544+beurmuz@users.noreply.github.com>
2023-06-20 15:14:33 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,14 @@
-print('이것은 연습이다')
+import requests
+from bs4 import BeautifulSoup
+
+url = 'https://ridibooks.com/category/new-releases/2200'
+response = requests.get(url)
+response.encoding = 'utf-8'
+html = response.text
+
+soup = BeautifulSoup(html, 'html.parser')
+
+#__next > main > div > section > ul.fig-1rl9mz1 > li:nth-child(1) > div > div.fig-smbj6f > div > div:nth-child(1) > a
+bookservices = soup.select('.fig-rs5q24')
+for no, book in enumerate(bookservices, 1):
+    print(no, book.text.strip())
